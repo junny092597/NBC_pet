e2f24651fa60b80b684fdc8a6b2ace60a12cbbce
ahneun <146177452+ahneun@users.noreply.github.com>
2024-01-11 19:20:58 +0900

    feat:styles 추가가

--- pet/src/redux/Store.tsx
+++ pet/src/redux/Store.tsx
@@ -1,7 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { AuthSlice } from './modules/authSlice'
+
 
 const Store = configureStore({
-  reducer: {},
+  reducer: {
+    AuthSlice: AuthSlice.reducer,},
 });
+
+export type RootState = ReturnType<typeof Store.getState>;
+export type AppDispatch = typeof Store.dispatch;
 
 export default Store;
---
+++ pet/src/redux/modules/AuthSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+import 
+
+const initialState = {
+    isLogin
+}
+
+const AuthSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+
+        }  
+    }
+})
