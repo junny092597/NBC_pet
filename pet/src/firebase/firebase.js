4647fdfe6e48ad60d701249e3e0ecc29f32e24ea
HONGSANGYOON <jay182@naver.com>
2024-01-18 17:52:14 +0900

    Feature: 파이어베이스 key lacal화

--- pet/src/firebase/firebase.js
+++ pet/src/firebase/firebase.js
@@ -9,13 +9,13 @@ import { getAnalytics } from 'firebase/analytics';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBXcIqpaIgzneSgRW0UgIdGIEj10wgILQE',
-  authDomain: 'finalprojex.firebaseapp.com',
-  projectId: 'finalprojex',
-  storageBucket: 'finalprojex.appspot.com',
-  messagingSenderId: '807311215394',
-  appId: '1:807311215394:web:f77519debc024a9e12337b',
-  measurementId: 'G-1QHRJHEHS1',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
