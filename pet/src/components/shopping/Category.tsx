a996fb311f211144a97698790aba167d3446dc76
HONGSANGYOON <jay182@naver.com>
2024-01-18 16:47:15 +0900

    Feature: 고양이관련 데이터 연동

--- pet/src/components/shopping/Category.tsx
+++ pet/src/components/shopping/Category.tsx
@@ -5,7 +5,7 @@ import { db } from '../../firebase/firebase';
 
 interface Item {
   id: number;
-  Price: number;
+  가격: number;
   상품명: string;
   이미지: string;
 }
@@ -29,18 +29,124 @@ function Category({ selectedCategory, setSelectedCategory }: CategoryProps): JSX
   useEffect(() => {
     const fetchData = async (category: string) => {
       try {
-        const itemsCollection = collection(db, 'DogsFood');
-        const categoryQuery = query(itemsCollection);
+        // 초기화
+        setItemsData([]);
 
-        const itemsSnapshot = await getDocs(categoryQuery);
-        const itemsData: Item[] = itemsSnapshot.docs.map(doc => ({
-          id: doc.data().id,
-          Price: doc.data().Price,
-          상품명: doc.data().상품명,
-          이미지: doc.data().이미지,
-        }));
-        console.log({ itemsData });
-        setItemsData(itemsData);
+        if (category === '강아지') {
+          // DogsFood 컬렉션 데이터 가져오기
+          const dogsFoodCollection = collection(db, 'DogsFood');
+          const dogsFoodQuery = query(dogsFoodCollection);
+          const dogsFoodSnapshot = await getDocs(dogsFoodQuery);
+          const dogsFoodData: Item[] = dogsFoodSnapshot.docs.map(doc => ({
+            id: doc.data().id,
+            가격: doc.data().가격,
+            상품명: doc.data().상품명,
+            이미지: doc.data().이미지,
+          }));
+
+          // DogsSnack 컬렉션 데이터 가져오기
+          const dogsSnackCollection = collection(db, 'DogsSnack');
+          const dogsSnackQuery = query(dogsSnackCollection);
+          const dogsSnackSnapshot = await getDocs(dogsSnackQuery);
+          const dogsSnackData: Item[] = dogsSnackSnapshot.docs.map(doc => ({
+            id: doc.data().id,
+            가격: doc.data().가격,
+            상품명: doc.data().상품명,
+            이미지: doc.data().이미지,
+          }));
+
+          // DogsPlay 컬렉션 데이터 가져오기
+          const dogsPlayCollection = collection(db, 'DogsPlay');
+          const dogsPlayQuery = query(dogsPlayCollection);
+          const dogsPlaySnapshot = await getDocs(dogsPlayQuery);
+          const dogsPlayData: Item[] = dogsPlaySnapshot.docs.map(doc => ({
+            id: doc.data().id,
+            가격: doc.data().가격,
+            상품명: doc.data().상품명,
+            이미지: doc.data().이미지,
+          }));
+
+          // 데이터 합치기
+          const allItemsData = [...dogsFoodData, ...dogsSnackData, ...dogsPlayData];
+          console.log({ allItemsData });
+          setItemsData(allItemsData);
+        } else if (category === '고양이') {
+          // CatsFood 컬렉션 데이터 가져오기
+          const catsFoodCollection = collection(db, 'CatsFood');
+          const catsFoodQuery = query(catsFoodCollection);
+          const catsFoodSnapshot = await getDocs(catsFoodQuery);
+          const catsFoodData: Item[] = catsFoodSnapshot.docs.map(doc => ({
+            id: doc.data().id,
+            가격: doc.data().가격,
+            상품명: doc.data().상품명,
+            이미지: doc.data().이미지,
+          }));
+
+          // CatsSnack 컬렉션 데이터 가져오기
+          const catsSnackCollection = collection(db, 'CatsSnack');
+          const catsSnackQuery = query(catsSnackCollection);
+          const catsSnackSnapshot = await getDocs(catsSnackQuery);
+          const catsSnackData: Item[] = catsSnackSnapshot.docs.map(doc => ({
+            id: doc.data().id,
+            가격: doc.data().가격,
+            상품명: doc.data().상품명,
+            이미지: doc.data().이미지,
+          }));
+
+          // CatsPlay 컬렉션 데이터 가져오기
+          const catsPlayCollection = collection(db, 'CatsPlay');
+          const catsPlayQuery = query(catsPlayCollection);
+          const catsPlaySnapshot = await getDocs(catsPlayQuery);
+          const catsPlayData: Item[] = catsPlaySnapshot.docs.map(doc => ({
+            id: doc.data().id,
+            가격: doc.data().가격,
+            상품명: doc.data().상품명,
+            이미지: doc.data().이미지,
+          }));
+
+          // 데이터 합치기
+          const allItemsData = [...catsFoodData, ...catsSnackData, ...catsPlayData];
+          console.log({ allItemsData });
+          setItemsData(allItemsData);
+        } else if (category === '그외') {
+          // CatsFood 컬렉션 데이터 가져오기
+          const catsFoodCollection = collection(db, 'CatsFood');
+          const catsFoodQuery = query(catsFoodCollection);
+          const catsFoodSnapshot = await getDocs(catsFoodQuery);
+          const catsFoodData: Item[] = catsFoodSnapshot.docs.map(doc => ({
+            id: doc.data().id,
+            가격: doc.data().가격,
+            상품명: doc.data().상품명,
+            이미지: doc.data().이미지,
+          }));
+
+          // CatsSnack 컬렉션 데이터 가져오기
+          const catsSnackCollection = collection(db, 'CatsSnack');
+          const catsSnackQuery = query(catsSnackCollection);
+          const catsSnackSnapshot = await getDocs(catsSnackQuery);
+          const catsSnackData: Item[] = catsSnackSnapshot.docs.map(doc => ({
+            id: doc.data().id,
+            가격: doc.data().가격,
+            상품명: doc.data().상품명,
+            이미지: doc.data().이미지,
+          }));
+
+          // CatsPlay 컬렉션 데이터 가져오기
+          const catsPlayCollection = collection(db, 'CatsPlay');
+          const catsPlayQuery = query(catsPlayCollection);
+          const catsPlaySnapshot = await getDocs(catsPlayQuery);
+          const catsPlayData: Item[] = catsPlaySnapshot.docs.map(doc => ({
+            id: doc.data().id,
+            가격: doc.data().가격,
+            상품명: doc.data().상품명,
+            이미지: doc.data().이미지,
+          }));
+
+          // 데이터 합치기
+          const allItemsData = [...catsFoodData, ...catsSnackData, ...catsPlayData];
+          console.log({ allItemsData });
+          setItemsData(allItemsData);
+        }
       } catch (error) {
         console.error('Firestore에서 아이템을 가져오는 중 에러 발생:', error);
       }
@@ -64,25 +170,31 @@ function Category({ selectedCategory, setSelectedCategory }: CategoryProps): JSX
     <SCategoryContainer>
       {categories.map(category => (
         <div key={category}>
-          <SItemButton onClick={() => onClickCategory(category)} active={category === selectedCategory}>
-            {category}
-          </SItemButton>
-          {category === selectedCategory &&
-            selectedItems.map(item => (
-              <SItemButton key={item} onClick={() => onClickItem(item)}>
-                {item}
-              </SItemButton>
-            ))}
-          {category === selectedCategory &&
-            itemsData.map(item => (
-              <SItemBox key={item.id} onClick={() => onClickItem(item)}>
-                <div>{item.상품명}</div>
-                <div>{item.Price}원</div>
-                <img src={item.이미지} alt={item.상품명} />
-              </SItemBox>
-            ))}
+          <SButtonContainer>
+            <SItemButton onClick={() => onClickCategory(category)} active={category === selectedCategory}>
+              {category}
+            </SItemButton>
+            {category === selectedCategory &&
+              selectedItems.map(item => (
+                <SItemButton key={item} onClick={() => onClickItem(item)}>
+                  {item}
+                </SItemButton>
+              ))}
+          </SButtonContainer>
         </div>
       ))}
+      <SItemBoxContainer>
+        {selectedCategory &&
+          itemsData.map(item => (
+            <SItemContainer key={item.id}>
+              <SItemBox onClick={() => onClickItem(item)}>
+                <img src={item.이미지} alt={item.상품명} />
+                <div>{item.상품명}</div>
+                <div>{item.가격}원</div>
+              </SItemBox>
+            </SItemContainer>
+          ))}
+      </SItemBoxContainer>
     </SCategoryContainer>
   );
 }
@@ -102,12 +214,18 @@ const SItemButton = styled.button<{ active?: boolean }>`
   cursor: pointer;
 `;
 
+const SCategoryBox = styled.div`
+  display: flex;
+  flex-wrap: wrap; // 아이템을 여러 행에 걸쳐 나열하도록 함
+  background-color: red;
+`;
 const SItemBox = styled.div`
-  width: 20vw;
-  height: 20vh;
+  display: flex;
+  flex-direction: column;
+  width: calc(30vw - 20px); // 1열당 3개씩 나열하려면 여백을 고려하여 조절
+  height: 30vh;
   border: 1px solid #ccc;
   padding: 10px;
-  margin-top: 5px;
   cursor: pointer;
 
   &:hover {
@@ -115,7 +233,26 @@ const SItemBox = styled.div`
   }
 
   img {
-    max-width: 100px;
-    max-height: 100px;
+    max-width: 350px;
+    max-height: 850px;
   }
+`;
+
+const SButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: space-between; // 버튼 간 여백을 최대화
+`;
+
+const SItemContainer = styled.div`
+  width: calc(30vw - 20px); // 1열당 3개씩 나열하려면 여백을 고려하여 조절
+  margin: 5px; // 각 아이템 간 여백 추가
+  gap: 10px;
+`;
+
+const SItemBoxContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap; // 부모 너비를 넘어가면 다음 줄로 넘어가도록 함
+  gap: 10px; // 아이템 간 여백 조절
 `;
