764ac35e5915cd4f5bcf2267396bbf5d9f7d05ff
HONGSANGYOON <jay182@naver.com>
2024-01-21 21:32:19 +0900

    feature: 데이터베이스 중복 삭제

--- pet/src/Firebase.ts
+++ pet/src/Firebase.ts
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
-
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,6 +11,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
