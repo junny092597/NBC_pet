21b1d694cff938b1b636b51fa03ac1fc33d59fef
sekwan94 <papais3268@naver.com>
2024-01-25 18:23:43 +0900

    쇼츠 등록

--- pet/src/pages/Community.tsx
+++ pet/src/pages/Community.tsx
@@ -1,15 +1,15 @@
-// pages/Community.tsx
-// pages/Community.tsx
 import React from 'react';
 import Layout from '../components/community/Layout';
 import DailyBoard from '../components/community/DailyBoard';
 import QuestionBoard from '../components/community/QuestionBoard';
+import Youtubeshorts from '../components/community/Youtubeshorts';
 
 const Community = () => {
   return (
     <Layout>
       <DailyBoard />
       <QuestionBoard />
+      <Youtubeshorts />
     </Layout>
   );
 };
