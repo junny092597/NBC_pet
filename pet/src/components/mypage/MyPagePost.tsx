15e60e12bf4939683e78dac8323edd73a6253e63
ahneun <146177452+ahneun@users.noreply.github.com>
2024-01-28 20:02:17 +0900

    마이페이지 등록게시글수정

--- pet/src/components/mypage/MyPagePost.tsx
+++ pet/src/components/mypage/MyPagePost.tsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { useRecoilValue } from 'recoil';
-import { userInfo } from '../../atom';
-import NoResults from './NoPosts';
 import * as S from './style';
 import { db } from '../../Firebase';
 import { auth } from '../../Firebase';
-import { collection, getDocs, where } from 'firebase/firestore';
-import test1 from '../../assets/images/logo.png';
+import { collection, getDocs } from 'firebase/firestore';
+import { Link } from 'react-router-dom';
 
 export type Review = {
   id: string;
@@ -16,14 +13,12 @@ export type Review = {
 };
 
 const MyPagePost = () => {
-  // const user = useRecoilValue(userInfo);
-  // const userInfos = user.userInfomation;
+
   const [reviews, setReviews] = useState<Review[]>([]);
   const [loading, setLoading] = useState(true);
-  // const userEmail = user.userInfomation.email;
   const [email, setEmail] = useState<any>(auth.currentUser?.email);
   const currentUserInfos = auth.currentUser; // 현재 로그인한 사용자의 정보들(파이어베이스)
-  const [currentUser, setCurrentUser] = useState<any>(''); // 현재 로그인한 사용자 가져오기
+  // const [currentUser, setCurrentUser] = useState<any>(''); // 현재 로그인한 사용자 가져오기
 
   useEffect(() => {
     const fetchReviews = async () => {
@@ -61,16 +56,12 @@ const MyPagePost = () => {
   return (
     <S.AllPostContainer>
       <S.TitleContainer>등록게시글</S.TitleContainer>
-      {reviews.length === 0 ? (
-        <NoResults />
-      ) : (
+      {reviews.length > 0 ? (
         <S.ReviewContainer>
           {reviews.map(review => (
             <S.PostContainer
               key={review.id}
-              // src={!imageURL ? test1:imageURL}>
-            // eslint-disable-next-line jsx-a11y/alt-text
-            ><img src={test1}/>
+            >
               <S.TextContainer>
                 <S.TextEmail> {review.email}</S.TextEmail>
                 <S.TextIndex> {review.index}</S.TextIndex>
@@ -79,6 +70,13 @@ const MyPagePost = () => {
             </S.PostContainer>
           ))}
         </S.ReviewContainer>
+      ) : (
+        <S.NoPostsContainer>
+          <h2>등록된 게시글이 없습니다</h2>
+          <Link to="Community">
+            <>게시글 등록하기</>
+          </Link>
+        </S.NoPostsContainer>
       )}
     </S.AllPostContainer>
   );
