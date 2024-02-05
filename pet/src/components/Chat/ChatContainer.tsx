830f3551ff60b688b7fcc0ccda10841fd57d6a27
junny092597 <junny092597@naver.com>
2024-02-05 10:58:39 +0900

    채팅 구현중

--- pet/src/components/Chat/ChatContainer.tsx
+++ pet/src/components/Chat/ChatContainer.tsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import ChatMessageList from './ChatMessageList';
 import ChatInput from './ChatInput';
-import { Message } from './types';
+import { Message } from '../../types/interface';
 
 // Socket.IO 서버 주소를 지정합니다. 실제 배포 시에는 해당 서버 주소를 사용합니다.
-const socket = io('http://localhost:4000');
+const socket = io('http://localhost:3000');
 
 const ChatContainer: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
